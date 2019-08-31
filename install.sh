dc21213aed45d4eee15e71641ebff632580439e3
Error404 <35210339+error404-notfound@users.noreply.github.com>
2019-08-31 01:41:52 -0300

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,28 @@
+apt-get install util-linux
+apt-get install xterm
+unzip Imagenes
+cp -r Imagenes /bin
+cp -r APKS /bin
+rm -rf fotos
+clear
+clear
+clear
+clear
+setterm -foreground green
+chmod 777 thepayloadghost
+chmod 777 E404-x
+mv thepayloadghost /bin
+mv E404-x /bin
+apt-get install default-jdk -y
+apt-get install openjdk-8-jre-headless -y
+apt-get install openjdk-8-jdk -y
+apt-get install default-jre -y
+apt-get install update && apt-get upgrade -y 
+apt-get install zipalign -y
+setterm -foregroun yellow
+sudo apt-get install metasploit-framework
+sudo apt-get install apktool
+echo ""
+setterm -foreground white
+echo "ThePayloadGhost se encuentra en tu carpeta de binarios, correlo con thepayloadghost"
+echo ""
